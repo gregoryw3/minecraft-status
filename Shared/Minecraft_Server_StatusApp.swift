be151d7a02a6e4263aac6319bbac7ebf3cc6139d
Gregory Wainer <greg.wainer@gmail.com>
2022-05-16 20:33:42 -0400

    Initial Commit

---
+++ Shared/Minecraft_Server_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minecraft_Server_StatusApp.swift
+//  Shared
+//
+//  Created by Gregory Wainer on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Minecraft_Server_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
